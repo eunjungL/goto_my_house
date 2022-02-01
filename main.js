8bc7ce0868bd943d0cbc4be972efe322b1943464
eunjungL <dldmswjd322@gmail.com>
2022-02-02 00:28:30 +0900

    main/ signup link

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+function main_template() {
+    return `
+        <!DOCTYPE HTML>
+        <html>
+            <head>
+                <title>우리 집으로 가자</title>
+            </head>
+            <body>
+                <a href="/signup">회원 가입</a>
+            </body>
+        </html>
+    `;
+}
+
 app.get('/', function(req, res) {
-    res.send('first init');
+    res.send(main_template());
 })
 
 app.listen(3000);
