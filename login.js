759be8b863b20cc99f6129dce629b9811ea1dcd4
eunjungL <dldmswjd322@gmail.com>
2022-02-03 23:49:45 +0900

    login/ naver login api connect

--- login.js
+++ login.js
@@ -3,9 +3,17 @@ const app = express.Router();
 const db = require('./db');
 const bodyParser = require("body-parser");
 
+const Naver = {
+    client_id : `N4qicuxNXkUCLmil_Fs5`,
+    client_secret: `OsMQcEcYH0`,
+    state: `random`,
+    redirectURI: encodeURI(`http://localhost:3000/login/naver/callback`)
+}
+const naver_api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${Naver.client_id}&redirect_uri=${Naver.redirectURI}&state=${Naver.state}`;
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
-function login_template() {
+function login_template(naver_api_url) {
     return `
         <!DOCTYPE HTML>
         <html lang="ko" xmlns="http://www.w3.org/1999/html">
@@ -20,6 +28,7 @@ function login_template() {
                     <label for="password">비밀번호</label><br>
                     <input type="password" id="password" name="password"><br>
                     <input type="submit" id="login_button" value="로그인">
+                    <a href=${naver_api_url}>네이버로 로그인</a>
                 </form>
             </body>
         </html>
@@ -27,7 +36,7 @@ function login_template() {
 }
 
 app.get('/', async (req, res) => {
-    res.send(login_template());
+    res.send(login_template(naver_api_url));
 })
 
 module.exports = app;
