3f7c0c66c44770fa0b1d9f8782fcd85587f21371
eunjungL <dldmswjd322@gmail.com>
2022-02-04 11:45:00 +0900

    login merge with master

---
+++ admin.js
@@ -0,0 +1,14 @@
+const Naver = {
+    client_id : `N4qicuxNXkUCLmil_Fs5`,
+    client_secret: `OsMQcEcYH0`,
+    state: `random`,
+    redirectURI: encodeURI(`http://localhost:3000/login/naver/callback`)
+}
+
+const Twilio = {
+    account_sid: 'AC55664512fd21c89eab8a88fc06d4ad9e',
+    auth_token: `b95839be05eedc2067100156d3bb744d`,
+    messaging_service_sid: 'MGdbcd196b24bb1790652ecf252eecb476'
+}
+
+module.exports = {Naver, Twilio};
